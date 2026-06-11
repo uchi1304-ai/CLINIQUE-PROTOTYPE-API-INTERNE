22aa2764039314d6ef10ccc3db80ae0ff6b3ee76
Ursula Calderon <ursula.calderon.curay@gmail.com>
2026-06-11 07:17:21 +0200

    Création des tables SQLite et insertion des données initiales

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  mail TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  role TEXT NOT NULL
+);
+INSERT INTO users (mail, password, role)
+VALUES
+('admin@cliniqueplus.fr', 'azerty', 'admin'),
+('secretaire@cliniqueplus.fr', 'azerty', 'staff');
